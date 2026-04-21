d228fa8d8cd2caf5bc4530b5869897106406e3bb
gpb-codes <g.pedreros.becerra@gmail.com>
2026-04-21 13:06:40 -0400

    Refactor PIN validation logic to check length instead of range

--- ejerecicios en clase/pin.py
+++ ejerecicios en clase/pin.py
@@ -1,6 +1,6 @@
 pin = int(input("Crea un pin de 4 dígitos: "))
 
-if 1000 <= pin <= 9999:
+if len(str(pin)) == 4:
     print("Pin válido")
 else:
     print("El pin debe tener exactamente 4 dígitos")
